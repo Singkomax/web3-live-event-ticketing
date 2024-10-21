2fd602257150e0bac2f7959015df279eedb9e0f6
James Russell Orola <jrmorola@gmail.com>
2024-10-21 09:56:45 +0800

    Move prettierrc to js projects, format contracts using forge

--- foundry/src/EventTicket.sol
+++ foundry/src/EventTicket.sol
@@ -1,74 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
-import { ERC721 } from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import { ERC721URIStorage } from '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-import { Strings } from '@openzeppelin/contracts/utils/Strings.sol';
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 contract EventTicket is ERC721URIStorage, Ownable {
-  using Strings for uint256;
+    using Strings for uint256;
 
-  uint256 private _nextTokenId;
-  string private _baseTokenURI;
+    uint256 private _nextTokenId;
+    string private _baseTokenURI;
 
-  struct TicketType {
-    string name;
-    uint256 price;
-    uint256 quantity;
-  }
-
-  // Mapping from ticket type to its details
-  mapping(string => TicketType) public ticketTypes;
-
-  // Mapping from tokenId to ticket type (to track which ticket type an NFT belongs to)
-  mapping(uint256 => string) public ticketTypeOfToken;
-
-  // Event information
-  // string public eventName; -> stored in the name of the ERC721 token
-  string public eventDescription;
-  uint256 public eventDate;
-  address public eventOrganizer;
-
-  constructor(
-    string memory _eventName,
-    string memory _eventDescription,
-    uint256 _eventDate,
-    address _eventOrganizer,
-    string memory _baseURI
-  ) ERC721(_eventName, 'EVT') Ownable(_eventOrganizer) {
-    eventDescription = _eventDescription;
-    eventDate = _eventDate;
-    eventOrganizer = _eventOrganizer;
-    _baseTokenURI = _baseURI;
-  }
-
-  function mintTickets(TicketType[] memory _ticketTypes) public onlyOwner {
-    if (_nextTokenId > 0) {
-      revert('Tickets already minted');
+    struct TicketType {
+        string name;
+        uint256 price;
+        uint256 quantity;
     }
 
-    // Set up the ticket types and pre-mint the tickets
-    for (uint256 i = 0; i < _ticketTypes.length; i++) {
-      ticketTypes[_ticketTypes[i].name] = _ticketTypes[i];
+    // Mapping from ticket type to its details
+    mapping(string => TicketType) public ticketTypes;
 
-      string memory ticketTypeURI = string.concat(
-        _baseTokenURI,
-        _ticketTypes[i].name,
-        '/'
-      );
+    // Mapping from tokenId to ticket type (to track which ticket type an NFT belongs to)
+    mapping(uint256 => string) public ticketTypeOfToken;
 
-      // Pre-mint all tickets of this type
-      for (uint256 j = 0; j < _ticketTypes[i].quantity; j++) {
-        _nextTokenId++;
-        _safeMint(eventOrganizer, _nextTokenId); // Mint to the event organizer
-        string memory tokenURI = string.concat(
-          ticketTypeURI,
-          _nextTokenId.toString()
-        );
-        _setTokenURI(_nextTokenId, tokenURI);
-        ticketTypeOfToken[_nextTokenId] = _ticketTypes[i].name;
-      }
+    // Event information
+    string public eventDescription;
+    uint256 public eventDate;
+    address public eventOrganizer;
+
+    constructor(
+        string memory _eventName,
+        string memory _eventDescription,
+        uint256 _eventDate,
+        address _eventOrganizer,
+        string memory _baseURI
+    ) ERC721(_eventName, "EVT") Ownable(_eventOrganizer) {
+        eventDescription = _eventDescription;
+        eventDate = _eventDate;
+        eventOrganizer = _eventOrganizer;
+        _baseTokenURI = _baseURI;
     }
-  }
+
+    function mintTickets(TicketType[] memory _ticketTypes) public onlyOwner {
+        if (_nextTokenId > 0) {
+            revert("Tickets already minted");
+        }
+
+        // Set up the ticket types and pre-mint the tickets
+        for (uint256 i = 0; i < _ticketTypes.length; i++) {
+            ticketTypes[_ticketTypes[i].name] = _ticketTypes[i];
+
+            // Pre-mint all tickets of this type
+            for (uint256 j = 0; j < _ticketTypes[i].quantity; j++) {
+                _nextTokenId++;
+                _mint(eventOrganizer, _nextTokenId); // Mint to the event organizer
+                ticketTypeOfToken[_nextTokenId] = _ticketTypes[i].name;
+            }
+        }
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        _requireOwned(tokenId);
+        string memory ticketType = ticketTypeOfToken[tokenId];
+        return
+            string.concat(_baseTokenURI, ticketType, "/", tokenId.toString());
+    }
 }
--- foundry/test/EventTicket.t.sol
+++ foundry/test/EventTicket.t.sol
@@ -1,105 +1,115 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { Test, console } from 'forge-std/Test.sol';
-import { EventTicket } from '../src/EventTicket.sol';
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import {Test, console} from "forge-std/Test.sol";
+import {EventTicket} from "../src/EventTicket.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EventTicketTest is Test {
-  EventTicket public eventTicket;
-  address public organizer = vm.addr(1);
+    EventTicket public eventTicket;
+    address public organizer = vm.addr(1);
 
-  function setUp() public {
-    eventTicket = new EventTicket(
-      'Making your first NFT',
-      'Learn how to make your first NFT',
-      1719849600,
-      organizer,
-      'https://mydomain.com/tickets/'
-    );
-  }
+    function setUp() public {
+        eventTicket = new EventTicket(
+            "Making your first NFT",
+            "Learn how to make your first NFT",
+            1719849600,
+            organizer,
+            "https://mydomain.com/tickets/"
+        );
+    }
 
-  function test_EventInfo() public view {
-    assertEq(eventTicket.name(), 'Making your first NFT');
-    assertEq(eventTicket.symbol(), 'EVT');
-    assertEq(
-      eventTicket.eventDescription(),
-      'Learn how to make your first NFT'
-    );
-    assertEq(eventTicket.eventDate(), 1719849600);
-    assertEq(eventTicket.eventOrganizer(), organizer);
-  }
+    function test_EventInfo() public {
+        assertEq(eventTicket.name(), "Making your first NFT");
+        assertEq(eventTicket.symbol(), "EVT");
+        assertEq(
+            eventTicket.eventDescription(),
+            "Learn how to make your first NFT"
+        );
+        assertEq(eventTicket.eventDate(), 1719849600);
+        assertEq(eventTicket.eventOrganizer(), organizer);
+    }
 
-  function test_MintTickets() public {
-    EventTicket.TicketType[] memory ticketTypes = new EventTicket.TicketType[](
-      3
-    );
+    function test_MintTickets() public {
+        EventTicket.TicketType[]
+            memory ticketTypes = new EventTicket.TicketType[](3);
 
-    ticketTypes[0] = EventTicket.TicketType({
-      name: 'Regular',
-      price: 100 ether,
-      quantity: 5
-    });
+        ticketTypes[0] = EventTicket.TicketType({
+            name: "Regular",
+            price: 100 ether,
+            quantity: 5
+        });
 
-    ticketTypes[1] = EventTicket.TicketType({
-      name: 'Special',
-      price: 150 ether,
-      quantity: 5
-    });
+        ticketTypes[1] = EventTicket.TicketType({
+            name: "Special",
+            price: 150 ether,
+            quantity: 5
+        });
 
-    ticketTypes[2] = EventTicket.TicketType({
-      name: 'VIP',
-      price: 200 ether,
-      quantity: 5
-    });
+        ticketTypes[2] = EventTicket.TicketType({
+            name: "VIP",
+            price: 200 ether,
+            quantity: 5
+        });
 
-    vm.prank(organizer);
-    eventTicket.mintTickets(ticketTypes);
-    vm.stopPrank();
+        vm.prank(organizer);
+        eventTicket.mintTickets(ticketTypes);
+        vm.stopPrank();
 
-    assertEq(eventTicket.balanceOf(organizer), 15);
-    assertEq(eventTicket.ticketTypeOfToken(1), 'Regular');
-    assertEq(eventTicket.ticketTypeOfToken(6), 'Special');
-    assertEq(eventTicket.ticketTypeOfToken(11), 'VIP');
-    assertEq(eventTicket.tokenURI(1), 'https://mydomain.com/tickets/Regular/1');
-    assertEq(eventTicket.tokenURI(6), 'https://mydomain.com/tickets/Special/6');
-    assertEq(eventTicket.tokenURI(11), 'https://mydomain.com/tickets/VIP/11');
-  }
+        assertEq(eventTicket.balanceOf(organizer), 15);
+        assertEq(eventTicket.ticketTypeOfToken(1), "Regular");
+        assertEq(eventTicket.ticketTypeOfToken(6), "Special");
+        assertEq(eventTicket.ticketTypeOfToken(11), "VIP");
+        assertEq(
+            eventTicket.tokenURI(1),
+            "https://mydomain.com/tickets/Regular/1"
+        );
+        assertEq(
+            eventTicket.tokenURI(6),
+            "https://mydomain.com/tickets/Special/6"
+        );
+        assertEq(
+            eventTicket.tokenURI(11),
+            "https://mydomain.com/tickets/VIP/11"
+        );
+    }
 
-  function test_RevertWhenNotOwner() public {
-    EventTicket.TicketType[] memory ticketTypes = new EventTicket.TicketType[](
-      1
-    );
-    ticketTypes[0] = EventTicket.TicketType('General Admission', 100, 1 ether);
+    function test_RevertWhenNotOwner() public {
+        EventTicket.TicketType[]
+            memory ticketTypes = new EventTicket.TicketType[](1);
+        ticketTypes[0] = EventTicket.TicketType(
+            "General Admission",
+            100,
+            1 ether
+        );
 
-    vm.expectRevert(
-      abi.encodeWithSelector(
-        Ownable.OwnableUnauthorizedAccount.selector,
-        address(this)
-      )
-    );
-    eventTicket.mintTickets(ticketTypes);
-  }
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector,
+                address(this)
+            )
+        );
+        eventTicket.mintTickets(ticketTypes);
+    }
 
-  function test_RevertWhenAlreadyMinted() public {
-    EventTicket.TicketType[] memory ticketTypes = new EventTicket.TicketType[](
-      1
-    );
+    function test_RevertWhenAlreadyMinted() public {
+        EventTicket.TicketType[]
+            memory ticketTypes = new EventTicket.TicketType[](1);
 
-    ticketTypes[0] = EventTicket.TicketType({
-      name: 'Regular',
-      price: 100 ether,
-      quantity: 5
-    });
+        ticketTypes[0] = EventTicket.TicketType({
+            name: "Regular",
+            price: 100 ether,
+            quantity: 5
+        });
 
-    // First mint will succeed
-    vm.prank(organizer);
-    eventTicket.mintTickets(ticketTypes);
+        // First mint will succeed
+        vm.prank(organizer);
+        eventTicket.mintTickets(ticketTypes);
 
-    // Further attempt to mint will revert
-    vm.prank(organizer);
-    vm.expectRevert('Tickets already minted');
-    eventTicket.mintTickets(ticketTypes);
-    vm.stopPrank();
-  }
+        // Further attempt to mint will revert
+        vm.prank(organizer);
+        vm.expectRevert("Tickets already minted");
+        eventTicket.mintTickets(ticketTypes);
+        vm.stopPrank();
+    }
 }
